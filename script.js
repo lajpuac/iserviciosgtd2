f9e6f003775687f9b3f37ec23dd5eb6b643372fe
lajpuac <43415701+lajpuac@users.noreply.github.com>
2025-02-03 21:46:37 -0600

    Cambios finales

---
+++ script.js
@@ -0,0 +1,14 @@
+let lastScrollTop = 0;
+const header = document.getElementById("header");
+
+window.addEventListener("scroll", function() {
+    let scrollTop = window.scrollY || document.documentElement.scrollTop;
+
+    if (scrollTop > lastScrollTop) {
+        header.classList.add("header-hidden");
+    } else {
+        header.classList.remove("header-hidden");
+    }
+
+    lastScrollTop = scrollTop;
+});
